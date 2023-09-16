37dc16deddd548239afa522ba578201d80a6c9d8
Shin123 <vanvietkhtn@gmail.com>
2023-09-16 20:54:21 +0700

    done Nike website

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import CustomerReviews from './CustomerReviews';
+import PopularProducts from './PopularProducts';
+import SupperQuality from './SupperQuality';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import Footer from './Footer';
+import Subscribe from './Subscribe';
+
+export {
+  Hero,
+  CustomerReviews,
+  PopularProducts,
+  SupperQuality,
+  Services,
+  SpecialOffer,
+  Footer,
+  Subscribe,
+};
